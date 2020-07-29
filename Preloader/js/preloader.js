6c843d2e7f4d9425e21664029ced9266a089743f
Robert Zhukov <robertzhukov97@mail.ru>
2020-07-30 00:34:13 +0300

    Update preloader.js

--- Preloader/js/preloader.js
+++ Preloader/js/preloader.js
@@ -1,19 +1,31 @@
-/* Закрашивание */
-let finish = 0;
-let start = -198;
-let speed = 0.6;
+let finishPaint = 0;
+let startPaint = -198;
+let speedPaint = 0.61;
+let xPosition = "x";
+const intervalBetweenFrames = 10;
+let speedUp = 5;
+let timeToFinishPaint = 500;
 
-let period = setInterval(function() {
-	maskRect.setAttribute('x', start)
-	  start = start + speed; 
-	  if(start > finish) {
-		clearInterval(period);
-	  }
-}, 10);
+// Fill logo
+let paintPreloader = setInterval(function () {
+  maskRect.setAttribute(xPosition, startPaint);
+  startPaint = startPaint + speedPaint;
 
-/* Отслеживание загрузки */
-window.onload = function() {
-	setTimeout(function() {
-		document.getElementById("preloader_load").style.display = "none";
-	}, 3380);
+  if (startPaint > finishPaint) {
+    clearInterval(paintPreloader);
+  }
+}, intervalBetweenFrames);
+
+// Tracking loading and finishing filling
+let finishPaintPreloader = setInterval(function () {
+  if (document.readyState === "complete") {
+    speedPaint = speedPaint + speedUp;
+    clearInterval(finishPaintPreloader);
+  }
+}, intervalBetweenFrames);
+
+window.onload = function () {
+  setTimeout(function () {
+    document.getElementById("preloader").style.display = "none";
+  }, timeToFinishPaint);
 };
