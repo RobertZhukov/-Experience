f56f71a4ce638708bc900d150cad9aec5221871d
Robert Zhukov <robertzhukov97@mail.ru>
2020-08-05 13:14:03 +0300

    Update preloader.js

--- Preloader/js/preloader.js
+++ Preloader/js/preloader.js
@@ -5,60 +5,38 @@ let timer = setInterval(() => {
   time = time + 1;
 }, timeBetweenLetters);
 
+const letterFill = (id) => {
+  id.setAttribute("fill", "#BA2425");
+};
+
 let paintSymbal = setInterval(() => {
   switch (time) {
     case 1:
-      primer11.setAttribute("fill", "#BA2425");
-      primer12.setAttribute("fill", "#BA2425");
-      primer13.setAttribute("fill", "#BA2425");
+      letterFill(letter_1);
       break;
 
     case 2:
-      primer21.setAttribute("fill", "#BA2425");
-      primer22.setAttribute("fill", "#BA2425");
-      primer23.setAttribute("fill", "#BA2425");
+      letterFill(letter_2);
       break;
 
     case 3:
-      primer31.setAttribute("fill", "#BA2425");
-      primer32.setAttribute("fill", "#BA2425");
-      primer33.setAttribute("fill", "#BA2425");
-      primer34.setAttribute("fill", "#BA2425");
+      letterFill(letter_3);
       break;
 
     case 4:
-      primer41.setAttribute("fill", "#BA2425");
-      primer42.setAttribute("fill", "#BA2425");
-      primer43.setAttribute("fill", "#BA2425");
-      primer44.setAttribute("fill", "#BA2425");
-      primer45.setAttribute("fill", "#BA2425");
-      primer46.setAttribute("fill", "#BA2425");
-      primer47.setAttribute("fill", "#BA2425");
-      primer48.setAttribute("fill", "#BA2425");
-      primer49.setAttribute("fill", "#BA2425");
-      primer410.setAttribute("fill", "#BA2425");
-      primer411.setAttribute("fill", "#BA2425");
+      letterFill(letter_4);
       break;
 
     case 5:
-      primer51.setAttribute("fill", "#BA2425");
-      primer52.setAttribute("fill", "#BA2425");
-      primer53.setAttribute("fill", "#BA2425");
-      primer54.setAttribute("fill", "#BA2425");
-      primer55.setAttribute("fill", "#BA2425");
+      letterFill(letter_5);
       break;
 
     case 6:
-      primer61.setAttribute("fill", "#BA2425");
-      primer62.setAttribute("fill", "#BA2425");
-      primer63.setAttribute("fill", "#BA2425");
+      letterFill(letter_6);
       break;
 
     case 7:
-      primer71.setAttribute("fill", "#BA2425");
-      primer72.setAttribute("fill", "#BA2425");
-      primer73.setAttribute("fill", "#BA2425");
-      primer74.setAttribute("fill", "#BA2425");
+      letterFill(letter_7);
       break;
 
     case 8:
